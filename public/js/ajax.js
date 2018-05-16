1ccfd08609f1c296acce5da1e6a3bfe6c2ebc0e9
Seth Marini <sethmarini@gmail.com>
2018-05-16 18:05:02 -0400

    updated ajax x2

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,25 +1,49 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	$('form').eq(0).submit(function(e) {
+	$('form').eq(0).submit(function (e) {
 		e.preventDefault();
-		var task	 	= $('#task-menu').find('.active').text(),
-			name       	= $('.formName').val().trim(),
-			description = $('.formDescription').val().trim(),
-			start       = $('.formStart').val().trim(),
-			duration    = $('.formDuration').val().trim();
+		var category = $('#pick-a-service').find('a.active').find('span').text().trim();
 		
+		switch (category) {
+			case 'Health':
+				var name        = $('#healthName').val().trim(),
+					description = $('#healthDescription').val().trim(),
+					start       = $('#healthStart').val().trim(),
+					duration    = $('#healthDuration').val().trim();
+				break;
+			case 'Work':
+				var name        = $('#workName').val().trim(),
+					description = $('#workDescription').val().trim(),
+					start       = $('#workStart').val().trim(),
+					duration    = $('#workDuration').val().trim();
+				break;
+			case 'Recreational':
+				var name        = $('#recName').val().trim(),
+					description = $('#recDescription').val().trim(),
+					start       = $('#recStart').val().trim(),
+					duration    = $('#recDuration').val().trim();
+				break;
+			case 'Other':
+			
+				var name        = $('#otherName').val().trim(),
+					description = $('#otherDescription').val().trim(),
+					start       = $('#otherStart').val().trim(),
+					duration    = $('#otherDuration').val().trim();
+				break;
+		};
+
 		$.ajax({
 			type: 'POST',
 			url: '/api/tasks',
 			data: {
-				task: task,
+				category: category,
 				name: name,
 				description: description,
 				start: start,
 				duration: duration
 			}
-		}).done(function() {
-			alert('Form Submitted!');
+		}).then(function (data) {
+			console.log('done');
 		});
 	});
 });
